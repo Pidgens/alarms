7ebddb990fecac30bc834a0fd88e2bef324066d6
Pidgens <dederekchiu@berkeley.edu>
2016-04-06 19:19:57 -0700

    make alarm ring infinitely long

--- app/src/main/java/com/example/derekchiu/alarm/AlarmReceiver.java
+++ app/src/main/java/com/example/derekchiu/alarm/AlarmReceiver.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
+import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.content.WakefulBroadcastReceiver;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.Random;
 
 /**
  * Created by derekchiu on 12/29/15.
@@ -21,42 +23,57 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
     private TextView val1;
     private TextView val2;
     private EditText inputVal;
+    private boolean isRinging = false;
+    private ToggleButton toggleButton;
+    private int myValue;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
         MainActivity inst = MainActivity.instance();
+        toggleButton = (ToggleButton) inst.findViewById(R.id.toggleButton);
         val1 = (TextView) inst.findViewById(R.id.val1);
         val2 = (TextView) inst.findViewById(R.id.val2);
         inputVal = (EditText) inst.findViewById(R.id.inputVal);
+        isRinging = true;
+        myValue = 0;
+        val1.setVisibility(View.VISIBLE);
+        val2.setVisibility(View.VISIBLE);
+        inputVal.setVisibility(View.VISIBLE);
+
+        Random r1 = new Random();
+        int i1 = r1.nextInt(500-100) + 100;
+        val1.setText(Integer.toString(i1));
+
+        Random r2 = new Random();
+        int i2 = r2.nextInt(500-100) + 100;
+        val2.setText(Integer.toString(i2));
+
+        int endVal = i1 + i2;
+
         Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         if (alarmUri == null) {
             alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         }
         final boolean toggled = intent.getExtras().getBoolean("toggled");
-        final Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-        if (!ringtone.isPlaying()) {
-            Log.i("RINGTONE IS PLAYING", "START");
-            ringtone.play();
-            val1.setVisibility(View.VISIBLE);
-            val2.setVisibility(View.VISIBLE);
-            inputVal.setVisibility(View.VISIBLE);
-        }
-        final Thread thread = new Thread() {
-            @Override
-            public void run() {
-                    if (toggled) {
-                        try {
-                            sleep(10000);
-                            ringtone.stop();
-                            Log.i("RINGTONE IS NOT PLAYING", "STOP");
+//        final Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        inputVal.setText(String.valueOf(0));
 
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-        };
-        thread.start();
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        MediaPlayer player = MediaPlayer.create(inst, notification);
+        player.setLooping(true);
+        player.start();
+//        ringtone.play();
+        if (isRinging) {
+//            Log.i("RINGTONE IS PLAYING", "START");
+            myValue = Integer.valueOf(inputVal.getText().toString());
+            if (myValue == endVal) {
+                isRinging = false;
+                toggleButton.setChecked(false);
+                player.stop();
+            }
+        }
+
         ComponentName componentName = new ComponentName(context.getPackageName(), AlarmService.class.getName());
         startWakefulService(context, (intent.setComponent(componentName)));
         setResultCode(Activity.RESULT_OK);
